bafce55d64baae6c859b7d95d4c4455788d941c6
chienhm <chienits@its-global.vn>
2022-12-07 03:44:15 +0000

    add validation pipeline

---
+++ src/Application/ApplicationServicesConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class ApplicationServicesConfiguration
+{
+  public static IServiceCollection ConfigureApplicationService(this IServiceCollection services)
+  {
+    var currentAssembly = typeof(ApplicationServicesConfiguration).Assembly;
+
+    services.AddMediatR(currentAssembly);
+    services.AddValidatorsFromAssembly(currentAssembly);
+
+    return services;
+  }
+}
---
+++ src/Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Application.Behaviors;
+
+public class ValidateBehavior<TRequest, TReponse>
+  : IPipelineBehavior<TRequest, TReponse>
+  where TRequest : IRequest<TReponse>
+  //where TReponse : ActionResponse
+{
+  private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+  public ValidateBehavior(IEnumerable<IValidator<TRequest>> validators)
+  {
+    _validators = validators;
+  }
+
+  public async Task<TReponse> Handle(TRequest request,
+    RequestHandlerDelegate<TReponse> next,
+    CancellationToken cancellationToken)
+  {
+    if (!_validators.Any())
+      return await next();
+
+    List<object> errors = new();
+
+    foreach (var validator in _validators)
+    {
+      var validateResult = await validator.ValidateAsync(request, cancellationToken);
+
+      if (validateResult.IsValid)
+        continue;
+
+      var error = validateResult.Errors
+     .Select(e => new { Field = e.PropertyName, Message = e.ErrorMessage });
+
+      errors.AddRange(error);
+    }
+
+    if (errors.Count == 0)
+      return await next();
+
+    //var response = new FailedValidationResponse(errors);
+
+    return (dynamic)errors;
+  }
+}
